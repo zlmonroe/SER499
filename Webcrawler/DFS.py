24f552c0e71bc98695df7ac09305b3dbc462902f
zlmonroe <zlmonroe@asu.edu>
2018-01-23 13:52:14 -0700

    Added question in DFS.py

--- Webcrawler/DFS.py
+++ Webcrawler/DFS.py
@@ -1,22 +1,31 @@
 from Webcrawler import *
 from queue import LifoQueue
+import re
 
-
-class LastnameDFS(ASE.AbstractSearch):
+class DFS(AbstractSearchExample.AbstractSearch):
     """See AbstractSearchExample for documentation help"""
 
     def __init__(self, fringe):
         super().__init__(fringe)
 
     def search(self, root, goal, maxDepth=0):
-        # While BFS isn't exactly the first choice for implementing a webcrawler,
+        # 1. While BFS isn't exactly the first choice for implementing a webcrawler,
         # DFS is almost never used. What advantages does BFS have over DFS? Why
         # would DFS be impractical?
-        print("""
-        Students Response:
+        #
+        # 2. List the searches in order of which search would be best to use for this
+        # type of problem. Be sure to explain your though process on each as this is
+        # what will be graded, not your order. There are reasons for MOST orderings.
+        return re.sub("\n *","\n","""
+        Student's Responses:
+        **********Answer Below*****************
+        1.
+        
+        2.
+        ***************************************
         """)
 
 
 if __name__ == "__main__":
-    dfs = LastnameDFS(LifoQueue)
-    dfs.search("", "")
+    dfs = DFS(LifoQueue)
+    print(dfs.search("", ""))
