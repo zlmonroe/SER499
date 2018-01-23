f2e69c953ce897a09cbcaa35a3ec362d6711d8f3
zlmonroe <zlmonroe@asu.edu>
2018-01-22 17:16:35 -0700

    Removed solutions

--- WebcrawlerSolutions/EmptyDictionary.py
+++
@@ -1,8 +0,0 @@
-class EmptyDictionary(dict):
-    def __init__(self, default=set()):
-        super().__init__(self)
-        self.default = default
-
-    def __getitem__(self, idx):
-        self.setdefault(idx, self.default)
-        return dict.__getitem__(self, idx)
